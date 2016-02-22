9d65a2491e951a18b79ffb19cad2d71599cf0c48
renyuanc <chengry911@gmail.com>
2016-02-22 00:49:54 -0500

    Test

--- app/src/main/java/com/chfy/chfysports/LoginActivity.java
+++ app/src/main/java/com/chfy/chfysports/LoginActivity.java
@@ -56,7 +56,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private UserLoginTask mAuthTask = null;
 
-
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
